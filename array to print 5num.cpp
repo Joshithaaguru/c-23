c48f32f65b7626e9d8ec9c3003d6bb420fcec307
Joshithaaguru <joshithaaguru@gmail.com>
2025-03-11 21:24:24 +0530

    Add files via upload

---
+++ array to print 5num.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int num[5];
+	printf("enter the values of 5 numbers:");
+	for(int i=0;i<5;i++){
+		scanf("%d",&num[i]);
+	}
+	printf("the number you entered:");
+	for(int i=0;i<=5;i++){
+		printf("%d \n",num[i]);
+	}
+	printf("%d \n",num[2]);
+	return 0;
+}
